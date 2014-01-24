e8541358c2c7a1a6c94024264f9d9ef3d352362c
Reiner <enneth.19@gmail.com>
2014-01-25 00:11:22 +0800

    Additions as of 1-25-2014
    Insert Employee  - No photo yet, added server-side validations.
    Forwards to ManageEmployees.aspx after insert, which is not completed
    yet too...
    
    Tested and working. :)
    
    **Use latest version of AJAX Control Toolkit**

--- Admin/EmployeeReg.aspx.cs
+++ Admin/EmployeeReg.aspx.cs
@@ -18,17 +18,98 @@ public partial class Admin_EmployeeReg : System.Web.UI.Page
 
     }
 
+
+
+    private bool checkInputs()
+    {
+        //to check each for blank entries
+        if (txtFName.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (txtMName.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (txtLName.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (ddlGender.SelectedValue.Trim() == "")
+        {
+            return false;
+        }
+        if (txtBDay.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (txtContact.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (txtEmail.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (ddlAdminLevel.SelectedValue.Trim() == "")
+        {
+            return false;
+        }
+        if (txtUN.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (txtPwd1.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (txtPwd2.Text.Trim() == "")
+        {
+            return false;
+        }
+        if (txtDateEmployeed.Text.Trim() == "")
+        {
+            return false;
+        }
+
+
+        //checks if password 1 and 2 are similar
+        if (txtPwd1.Text.Trim() != txtPwd2.Text.Trim())
+        {
+            return false;
+        }
+
+        //checks if int and datetime objects are valid
+
+        try
+        {
+            DateTime.Parse(txtDateEmployeed.Text.Trim());
+            DateTime.Parse(txtBDay.Text.Trim());
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+
+    }
+
     protected void btnSubmit_Click(object sender, EventArgs e)
     {
-        SqlParameter[] checkUNParam = {
+
+        if (checkInputs())
+        {
+
+            SqlParameter[] checkUNParam = {
                                           new SqlParameter("@un", txtUN.Text)
                                       };
 
-        bool isExisting = DataAccess.DetermineIfExisting("SELECT * FROM Employees WHERE UN=@un", checkUNParam, conString);
-        if (isExisting != true)
-        {
-            string strInsert = "INSERT INTO Employees (FName, MName, LName, Gender, BDate, ContactNo, Email, AdminLevel, UN, PWD, DateOfEmployment) VALUES (@fname, @mname, @lname, @gender, @bdate, @contact, @email, @adminlevel, @un, @pwd, @doe)";
-            SqlParameter[] insrtParam = {
+            bool isExisting = DataAccess.DetermineIfExisting("SELECT * FROM Employees WHERE UN=@un", checkUNParam, conString);
+            if (isExisting != true)
+            {
+                string strInsert = "INSERT INTO Employees (FName, MName, LName, Gender, BDate, ContactNo, Email, AdminLevel, UN, PWD, DateOfEmployment) VALUES (@fname, @mname, @lname, @gender, @bdate, @contact, @email, @adminlevel, @un, @pwd, @doe)";
+                SqlParameter[] insrtParam = {
                                             new SqlParameter("@fname",  AntiXSSMethods.CleanString(txtFName.Text)),
                                             new SqlParameter("@mname", AntiXSSMethods.CleanString(txtMName.Text)),
                                             new SqlParameter("@lname", AntiXSSMethods.CleanString(txtLName.Text)),
@@ -41,28 +122,23 @@ public partial class Admin_EmployeeReg : System.Web.UI.Page
                                             new SqlParameter("@pwd", Encryption.MD5(AntiXSSMethods.CleanString((txtPwd2.Text)))),
                                             new SqlParameter("@doe", Convert.ToDateTime(txtDateEmployeed.Text))
                                         };
-            DataAccess.DataProcessExecuteNonQuery(strInsert, insrtParam, conString);
-            Response.Write("<script>alert('Success!');</script>");
+                DataAccess.DataProcessExecuteNonQuery(strInsert, insrtParam, conString);
+                //Response.Write("<script>alert('Success!');</script>");
+                Response.Redirect("ManageEmployees.aspx");
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Username already taken!');</script>");
+            }
+
+
+           
         }
         else
         {
-            Response.Write("<script>alert('Username already taken!');</script>");
-        }
-
-        if (IsPostBack)
-        {
-            txtFName.Text = "";
-            txtMName.Text = "";
-            txtLName.Text = "";
-            ddlGender.SelectedValue = "";
-            txtBDay.Text = "";
-            txtContact.Text = "";
-            txtEmail.Text = "";
-            ddlAdminLevel.SelectedValue = "";
-            txtUN.Text = "";
-            txtPwd1.Text = "";
-            txtPwd2.Text = "";
-            txtDateEmployeed.Text = "";
+            Response.Write("Some fields are either have invalid entries or left blank.");
         }
     }
+
 }
