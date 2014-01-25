380f9225e46c825565b704b218dcf76d6341fd3d
reinsmeister <enneth.19@gmail.com>
2014-01-25 13:28:28 +0800

    Try lang... hehe
    try... narevert ko kasi tae. hahaha

--- Admin/Default.aspx
+++ Admin/Default.aspx
@@ -3,6 +3,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="Head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Content" Runat="Server">
+<h1>Welcome to the Administrator's Page!</h1>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="Footer" Runat="Server">
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
 
 public partial class _Default : System.Web.UI.Page
 {
+
+    string ConnString = ConfigurationManager.ConnectionStrings["CONNSTRING"].ToString();
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
+    protected void btnLogIn_Click(object sender, EventArgs e)
+    {
+        
+    }
+   
+
 }
