3449a0e327f53fc7381809a8393a874996d17768
meganjpzon <potchie0o8@gmail.com>
2014-02-12 00:43:02 +0800

    Added Masterpages
    for Guardian and Tenant

---
+++ masterpages/Guardian.master.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using UserManagement;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblDate.Text = "Date: "+ DateTime.Now.Month.ToString() + "/" + DateTime.Now.Day.ToString() + "/" + DateTime.Now.Year.ToString();
+        CheckUser();
+    }
+
+
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+        Session.Clear();
+        Response.Redirect("~/Default.aspx");
+    }
+
+
+    private void CheckUser()
+    {
+        try
+        {
+            string Username = Tenants.ReturnUserName(int.Parse(Session["TenantID"].ToString()));
+            lblUsername.Text = Username;
+        }
+        catch
+        {
+            Session.Clear();
+            Response.Redirect("~/Default.aspx");
+        }
+    }
+
+}
