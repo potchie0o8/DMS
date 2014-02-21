34e4817872edabe96fe39656d9bf4a9967f9587d
reinermeister <enneth.19@gmail.com>
2014-02-21 13:56:45 +0800

    new shit
    yeeeah! Muh Profile en' sheeit, knowmsayin?

--- Admin/MyProfile.aspx.cs
+++ Admin/MyProfile.aspx.cs
@@ -4,11 +4,61 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DBHelpers;
+using System.Data.SqlClient;
+using Globals;
 
 public partial class Admin_MyProfile : System.Web.UI.Page
 {
+    string ConnString = StaticVariables.ConnectionString;
+
+    int EmployeeID;
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            EmployeeID = int.Parse(Session["EmployeeID"].ToString());
+            LoadData(EmployeeID);
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+
+    private void LoadData(int _EmployeeID) 
+    {
+        lblEmployeeID.Text = _EmployeeID.ToString();
+        string strSelect = "SELECT * FROM Employees WHERE EmployeeID=@EID";
+        SqlParameter[] EID = { new SqlParameter("@EID", _EmployeeID) };
+        SqlDataReader dr = DataAccess.ReturnReader(strSelect, EID, ConnString);
+        dr.Read();
+        lblAdminAccess.Text = IntToWords.getAdminAccess(int.Parse(dr["AdminLevel"].ToString()));
+        lblContactNo.Text = dr["ContactNo"].ToString();
+        lblDateOfBirth.Text = Convert.ToDateTime(dr["BDate"].ToString()).ToString("MMMM dd, yyyy");
+        lblDateOfEmployment.Text = dr["DateOfEmployment"].ToString();
+        lblEmailAdd.Text = dr["Email"].ToString();
+        lblFingerID.Text = dr["FingerprintID"].ToString();
+        lblFName.Text = dr["FName"].ToString();
+        lblGender.Text = IntToWords.getGender(int.Parse(dr["Gender"].ToString()));
+        lblLName.Text = dr["LName"].ToString();
+        lblMName.Text = dr["MName"].ToString();
+        lblUsername.Text = dr["UN"].ToString();
+        string photofile = dr["PhotoFile"].ToString();
+
+        if (photofile.Trim() == "" || photofile == null)
+        {
+            ImgMyPhoto.ImageUrl = "~/images/nophoto.jpg";
+        }
+        else
+        {
+            ImgMyPhoto.ImageUrl = "~/uploads/" + photofile;
+        }
+
+
+
+        DataAccess.ForceConnectionToClose();
+
 
     }
 
@@ -25,6 +75,7 @@ public partial class Admin_MyProfile : System.Web.UI.Page
   //    ,[Pwd]
   //    ,[DateOfEmployment]
   //    ,[PhotoFile]
+   //   ,[FingerprintID]
   //FROM [DMS_3].[dbo].[Employees]
 
 }
--- App_Code/Globals.cs
+++ App_Code/Globals.cs
@@ -13,4 +13,40 @@ namespace Globals
     {
         public static string ConnectionString = ConfigurationManager.ConnectionStrings["CONNSTRING"].ToString();
     }
+
+    public static class IntToWords
+    {
+        public static string getGender(int _genderID)
+        {
+            string gender = "";
+            switch (_genderID)
+            {
+                case 1:
+                    gender = "Male";
+                    break;
+                case 2:
+                    gender = "Female";
+                    break;
+            }
+            return gender;
+        }
+
+
+        public static string getAdminAccess(int _AccessLevel)
+        {
+            string AccessLevel = "";
+            switch (_AccessLevel)
+            {
+                case 1:
+                    AccessLevel = "<font color='green'>Full</font>";
+                    break;
+                case 2:
+                    AccessLevel = "<font color='red'>Limited</font>";
+                    break;
+            }
+            return AccessLevel;
+        }
+
+    }
+
 }
